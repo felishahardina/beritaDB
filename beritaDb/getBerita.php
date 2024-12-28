f5605ea2364b61d2dc17500a9726042944824010
felishahardina <felishahardina06@gmail.com>
2024-12-28 11:15:30 +0700

    beritaDB

---
+++ beritaDb/getBerita.php
@@ -0,0 +1,23 @@
+<?php
+header("Acces-Control-Allow-Origin: header");
+header("Acces-Control-Allow-Origin: *");
+
+include 'koneksi.php';
+
+    $sql = "SELECT * FROM berita";
+    $result = $koneksi->query($sql);
+    if($result->num_rows > 0) {
+        $response['isSucces'] = true;
+        $response['message'] = "Berhasil Menampilkan Berita";
+        $response['data'] = array();
+        while ($row = $result->fetch_assoc()) {
+            $response['data'][] = $row;
+        }
+    } else {
+        $response['isSucces']= false;
+        $response['message'] = "Gagal menampilkan berita";
+        $response['data'] = null;
+    }
+    echo json_encode($response);
+
+?>
